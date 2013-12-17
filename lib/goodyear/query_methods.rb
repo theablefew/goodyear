a34cf1ea798f0981a5f2cf2e090839f58cc9cf25
Spencer Markowski <spencer@theablefew.com>
2013-12-17 12:28:14 -0500

    Reset sort order

--- lib/goodyear/query_methods.rb
+++ lib/goodyear/query_methods.rb
@@ -20,10 +20,10 @@ module Goodyear
 
         tire = Tire::Search::Search.new(self.index_name, options)
         tire.query { string es.query } unless es.query.blank?
-        tire.sort{ by *es.sort } unless es.sort.nil? || es.sort.empty?
+        tire.sort{ by *es.sort } unless es.sort.blank?
         tire.size( es.size ) unless es.size.nil?
-        tire.fields( es.fields ) unless es.sort.nil? || es.fields.empty?
-        tire.highlight( es.highlights ) unless es.sort.nil? || es.highlights.empty?
+        tire.fields( es.fields ) unless es.fields.empty?
+        tire.highlight( es.highlights ) unless es.highlights.empty?
 
         es.facets.each do |f|
           tire.facet(f[:name], f[:args], &f[:l] )
@@ -65,6 +65,7 @@ module Goodyear
       @_fields = []
       @_and    = []
       @_size   =  nil
+      @_sort   = []
       @_or     = []
       @_facets = []
       @_highlights = []
